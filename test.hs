d8dad9ed6cbb49d375ed009035cd6fc3e4e7bd9b
James Higginson <Higginson27041992@gmail.com>
2018-09-28 20:27:59 +0100

    Initial Commit

---
+++ test.hs
@@ -0,0 +1,18 @@
+sayHello :: String -> IO ()
+sayHello x =
+    putStrLn ("Hello, " ++ x ++ "!")
+
+
+triple x = 3 * x
+
+half x = x / 2
+
+square x = x * x    
+
+squareAndMultiplyPi x = pi * (x * x)
+
+perimeter x y = (x * 2) + (y * 2)
+perimeter' x y  = x * 2 + y * 2
+
+f x = x / 2 + 9
+f' x = x / (2 + 9)
