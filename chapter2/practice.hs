c037303a9e418c439efb0a8ff85f22ab9583c53c
James Higginson <Higginson27041992@gmail.com>
2018-09-29 10:28:13 +0100

    Sort files into chapter order

---
+++ chapter2/practice.hs
@@ -0,0 +1,27 @@
+mult1       = x * y
+    where x = 5
+          y = 6
+
+mult2       = x * 3 + y
+    where x = 3
+          y = 1000
+
+
+mult3       = x * 5
+    where y = 10
+          x = 10 * 5 + y
+
+mult4       = z / x + y
+    where x = 7
+          y = negate x
+          z = y * 10
+
+
+waxOn       = x * 5
+    where x = y ^ 2
+          y = z + 8
+          z = 7
+
+triple x = 3 * x
+
+waxOff x = triple x
